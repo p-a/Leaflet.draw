cd88e9251a6a0340b3ebfaeaa48ea42c4b28934a
P-a <peba@metria.se>
2016-01-25 23:25:26 +0100

    latlntutil clones both flat and non-flat latlng arrays

--- spec/suites/LatLngUtilSpec.js
+++ spec/suites/LatLngUtilSpec.js
@@ -4,8 +4,46 @@ describe("L.LatLngUtil", function () {
 		var	clone = L.LatLngUtil.cloneLatLngs(latLngs);
 
 		expect(clone[0][0].lat).to.eql(latLngs[0][0].lat);
-		expect(clone[1][0].lat).to.eql(latLngs[1][0].lat);
+		expect(clone[1][0].lng).to.eql(2);
 		clone[0][0].lat = 10;
 		expect(latLngs[0][0].lat).to.eql(0);
 	});
 });
+
+describe("L.LatLngUtil", function () {
+	it("cloneLatLngs", function () {
+		var latLngs = [[L.latLng(0, 0)], [L.latLng(1, 2)]];
+		var	clone = L.LatLngUtil.cloneLatLngs(latLngs);
+
+		expect(clone[0][0].lat).to.eql(latLngs[0][0].lat);
+		expect(clone[1][0].lng).to.eql(2);
+		clone[0][0].lat = 10;
+		expect(latLngs[0][0].lat).to.eql(0);
+	});
+});
+
+
+describe("L.LatLngUtil", function () {
+	it("cloneLatLngs", function () {
+		var latLngs = [{ lat: 0, lng: 0 }, { lat: 1, lng: 2 }];
+		var	clone = L.LatLngUtil.cloneLatLngs(latLngs);
+
+		expect(clone[0].lat).to.eql(latLngs[0].lat);
+		expect(clone[1].lng).to.eql(2);
+		clone[0].lat = 10;
+		expect(latLngs[0].lat).to.eql(0);
+	});
+});
+
+describe("L.LatLngUtil", function () {
+	it("cloneLatLngs", function () {
+		var latLngs = [L.latLng(0, 0), L.latLng(1, 2)];
+		var	clone = L.LatLngUtil.cloneLatLngs(latLngs);
+
+		expect(clone[0].lat).to.eql(latLngs[0].lat);
+		expect(clone[1].lng).to.eql(2);
+		clone[0].lat = 10;
+		expect(latLngs[0].lat).to.eql(0);
+	});
+});
+
--- src/ext/LatLngUtil.js
+++ src/ext/LatLngUtil.js
@@ -5,13 +5,29 @@
 L.LatLngUtil = {
 	// Clones a LatLngs[], returns [][]
 	cloneLatLngs: function (latlngs) {
+		if (!latlngs) {
+			return null;
+		}
+
+		if (latlngs instanceof L.LatLng || !!latlngs.lat) {
+			return L.latLng(latlngs);
+		}
+
+		var flat = latlngs[0] instanceof L.LatLng || typeof latlngs[0] === 'object' && 'lat' in latlngs[0];
 		var clone = [];
-	    for (var j = 0, k = latlngs.length; j < k; j++) {
-			var temp = [];
-			for (var i = 0, l = latlngs[j].length; i < l; i++) {
-				temp.push(this.cloneLatLng(latlngs[j][i]));
+		var i, j;
+		if (flat) {
+			for (j = 0; j < latlngs.length; j++) {
+				clone.push(this.cloneLatLng(latlngs[j]));
 			}
-			clone.push(temp);
+		} else {
+			for (j = 0; j < latlngs.length; j++) {
+				var temp = [];
+				for (i = 0; i < latlngs[j].length; i++) {
+					temp.push(this.cloneLatLng(latlngs[j][i]));
+				}
+				clone.push(temp);
+			}
 		}
 		return clone;
 	},
